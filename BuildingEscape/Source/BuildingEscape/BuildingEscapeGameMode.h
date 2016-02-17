d32c05d610122a133ba6389850baaf3c01e14dfe
Ben Tristem <github@bentristem.com>
2016-02-17 11:32:56 +0000

    BE05 Your First .gitignore for Unreal

---
+++ BuildingEscape/Source/BuildingEscape/BuildingEscapeGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "BuildingEscapeGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BUILDINGESCAPE_API ABuildingEscapeGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
